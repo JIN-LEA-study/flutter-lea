bb1b951e31b459080e2fbaf88742e1a661bad6e6
leaisrevolution <leaisrevolution@gmail.com>
2022-05-04 01:45:33 +0900

    플러터 기본 위젯 넣는 법

--- codingapple/contact/lib/main.dart
+++ codingapple/contact/lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
+// 앱을 실행시켜주세요.
 void main() {
   runApp(const MyApp());
 }
 
+// 기본으로 채워져야 하는 문법
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    // Text : Text('안녕')'
+    // Icon : Icon.star
+    // Image : Image.assets('경로')
+    // Container,SizdBox(), LP단위
+
+    return MaterialApp(
+      home: Center( //부모
+        child: Container( width: 50, height: 50, color: Colors.blue) //자식
+      )
+    );
   }
+
 }
