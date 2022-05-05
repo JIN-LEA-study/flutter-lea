1e6d76d925b2c3503ba78784c8c52a06bfd791ca
leaisrevolution <leaisrevolution@gmail.com>
2022-05-05 21:01:51 +0900

    box border

--- codingapple/contact/lib/main.dart
+++ codingapple/contact/lib/main.dart
@@ -20,21 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar( title: Text('앱임')),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ]
+        body: Container(
+          width: 50, height: 50
+          // margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            border: Border.all(color : Colors.black)
+          )
             ),
           ),
-        ),
-      )
     );
   }
 }
