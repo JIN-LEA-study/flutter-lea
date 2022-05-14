d4eb3c4670048dcdede484ac74fa8bd53584535c
leaisrevolution <leaisrevolution@gmail.com>
2022-05-15 07:31:42 +0900

    패키지 세팅

--- codingapple/contact/lib/main.dart
+++ codingapple/contact/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart'
+import 'package:permission_handler/permission_handler.dart';
 
 // 앱을 실행시켜주세요.
 void main() {
